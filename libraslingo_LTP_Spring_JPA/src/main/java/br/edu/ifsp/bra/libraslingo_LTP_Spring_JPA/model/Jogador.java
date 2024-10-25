d7a5e0482d749bc42019fb6c42c084d7beb26186
ligras <gabigabs2608@gmail.com>
2024-10-25 14:58:19 -0300

    commit3

---
+++ libraslingo_LTP_Spring_JPA/src/main/java/br/edu/ifsp/bra/libraslingo_LTP_Spring_JPA/model/Jogador.java
@@ -0,0 +1,45 @@
+package br.edu.ifsp.bra.libraslingo_LTP_Spring_JPA.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Jogador {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private Long id;
+    private String nome;
+    private String senha;
+
+    public Jogador(){
+        
+    }
+    public Jogador(Long id, String nome, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.senha = senha;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}

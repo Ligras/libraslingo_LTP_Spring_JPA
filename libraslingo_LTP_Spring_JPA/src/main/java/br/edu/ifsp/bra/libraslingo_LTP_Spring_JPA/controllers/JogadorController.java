65fc23e5785b9ff6267dddac6032e954d773674d
ligras <gabigabs2608@gmail.com>
2024-10-25 15:07:09 -0300

    commit4

---
+++ libraslingo_LTP_Spring_JPA/src/main/java/br/edu/ifsp/bra/libraslingo_LTP_Spring_JPA/controllers/JogadorController.java
@@ -0,0 +1,5 @@
+package br.edu.ifsp.bra.libraslingo_LTP_Spring_JPA.controllers;
+
+public class JogadorController {
+    
+}
---
+++ libraslingo_LTP_Spring_JPA/src/main/java/br/edu/ifsp/bra/libraslingo_LTP_Spring_JPA/repository/JogadorRepository.java
@@ -0,0 +1,10 @@
+package br.edu.ifsp.bra.libraslingo_LTP_Spring_JPA.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.edu.ifsp.bra.libraslingo_LTP_Spring_JPA.model.Jogador;
+
+public interface JogadorRepository extends CrudRepository<Jogador, Long> {
+
+}
+

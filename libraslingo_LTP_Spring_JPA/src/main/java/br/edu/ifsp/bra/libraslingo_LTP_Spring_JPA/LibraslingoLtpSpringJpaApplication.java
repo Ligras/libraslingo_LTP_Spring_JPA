be24aa0ec30f3a23e5f9aa14a0de2a1ca54838c4
ligras <gabigabs2608@gmail.com>
2024-10-25 14:47:25 -0300

    commit1

---
+++ libraslingo_LTP_Spring_JPA/src/main/java/br/edu/ifsp/bra/libraslingo_LTP_Spring_JPA/LibraslingoLtpSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.bra.libraslingo_LTP_Spring_JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraslingoLtpSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraslingoLtpSpringJpaApplication.class, args);
+	}
+
+}
---
+++ libraslingo_LTP_Spring_JPA/src/test/java/br/edu/ifsp/bra/libraslingo_LTP_Spring_JPA/LibraslingoLtpSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.bra.libraslingo_LTP_Spring_JPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraslingoLtpSpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
